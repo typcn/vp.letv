45e6853d590bba5669a4939b470ffac83826f954
TYPCN <admin@typcn.com>
2015-09-30 15:38:17 +0800

    Add inject script

--- plugin/Inject.js
+++ plugin/Inject.js
@@ -1,4 +1,3 @@
-
 window.sendToPlugin = function(data){
     var request = new XMLHttpRequest();
     request.open('POST', 'http://localhost:23330/pluginCall', true);
@@ -6,38 +5,30 @@ window.sendToPlugin = function(data){
     request.send(JSON.stringify(data));
 }
 
-window.setMsg = function(text){
-    document.getElementById("player").innerHTML = '<div class="player_html5"><div class="picture" style="height:100%"><div style="line-height:460px;"><span style="font-size:18px">' + text + '</span></div></div></div>';
+window.setMsg = function(){
+    $("#fla_box_con").html('<div id="fla_box_p"> <div id="fla_box" style="width: 1121px; height: 474px; background: rgb(0, 0, 0);"><div style="width:1121px;height:474px;margin:auto; background:#000;overflow:hidden;"><div style="text-align:center; color:#aaa; line-height:20px; font-size:24px; padding-top:120px"><br><br><br><a href="javascript:showPlayWindow()" style="color:#fff">立即播放</a></div></div></div><a class="player_bg" href="javascript:;" style="display: none;"><img src="http://i0.letvimg.com/lc02_img/201505/28/15/45/ico_bg.png"></a><a class="ico_close" href="javascript:;" style="display: none;"></a></div>');
 }
-setTimeout(function(){
-    var str = window.location.href;
-    var re = /v_show\/id_(.*)\.html/;
-    var m = str.match(re);
-    if(m && m.length == 2){
-        window.ykvid = m[1];
-        setMsg('<a href="javascript:getVideoAddr()">立即播放</a>');
-    }
-},500);
 
-window.getVideoAddr = function(){
-    setMsg('<a href="#">正在解析视频地址</a>');
-    youkuParser(ykvid,function(d){
-        if(!d || d.length < 1){
-            setMsg('<a href="#">解析失败，返回内容为空</a>');
-        }else{
-            var str = "请选择画质: ";
-            window.videoAddr = d;
-            for(var i =0;i < d.length;i++){
-                str +='<a href="javascript:invokePlayer('+i+')">' + d[i][0] + '</a> ';
-            }
-            setMsg(str);
+
+window.showPlayWindow = function(){
+    sendToPlugin({ action:'letv-playvideo', data:window.levid });
+}
+
+if(!window.ins){
+    window.ins = setInterval(function(){
+        var str = window.location.href;
+        var re = /\/(\d+).html/;
+        var m = str.match(re);
+        if(m && m.length == 2){
+            window.levid = m[1];
+            setMsg();
         }
-    });
+        if(!window.loadTimes){
+            window.loadTimes = 1;
+        }else if(window.loadTime > 15){
+            clearInterval(ins);
+        }else{
+            window.loadTimes++;
+        }
+    },500);
 }
-
-window.invokePlayer = function(i){
-    var vaddr = window.videoAddr[i][1];
-    sendToPlugin({ action:'youku-playvideo', data:vaddr });
-}
-
-window.log = console.log;
